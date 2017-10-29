d24e27f81f870d0dac3afe8ca0217e330e1fae09
georgesoft07 <george_soft_07@hotmail.com>
2017-10-29 13:18:03 -0500

    first commit

---
+++ QUERY.sql
@@ -0,0 +1,32 @@
+CREATE BDEXAMEN
+GO
+USE [BDEXAMEN]
+GO
+
+/****** Object:  Table [dbo].[TbMuestra]    Script Date: 29/10/2017 1:16:25 p. m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[TbMuestra](
+	[Id] [int] NOT NULL,
+	[Nombre] [varchar](max) NULL,
+	[fecha] [datetime] NULL,
+	[NombrePersona] [varchar](50) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
